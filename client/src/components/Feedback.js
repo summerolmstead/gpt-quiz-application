db9129785528b7042b5ad7503c1337ded573ef96
Major <107882188+qubitflip@users.noreply.github.com>
2024-01-25 22:31:32 -0500

    Added date

--- client/src/components/Feedback.js
+++ client/src/components/Feedback.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Feedback = ({ feedback }) => {
     return <div className="feedback-box">{feedback}</div>;
-    // Future fix feedback box
+    // Future fix feedback box (do tomorrow)
 };
 
 export default Feedback;
